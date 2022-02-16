9172bc67251aaabc100e2ef1a48ea70db80c7edc
PerchunPak <68118654+PerchunPak@users.noreply.github.com>
2022-02-16 11:20:34 +0200

    Fix imports in cli.py

--- mc_plugin_helper/cli.py
+++ mc_plugin_helper/cli.py
@@ -1,7 +1,7 @@
 """Module for CLI commands."""
 
 from typing import List, Union
-from click import Path, command, echo, argument
+from click import Path, argument, command, echo
 from mc_plugin_helper.config import Config
 from mc_plugin_helper.plugin_manager import Plugin, PluginManager
 
@@ -33,6 +33,10 @@ class CLI(object):
         """__init__ method."""
         self.config = Config.init().config
         self._echo = NiceEcho
+
+    @group()
+    def cli(self):
+        """"""
 
     @command()
     @argument(
